76c50c6a020c8e65270c5d00c5db4fb6bdb2c7d9
Markus Rousu <rousu.markus@gmail.com>
2025-08-19 17:56:49 +0300

    Front end to render

--- osa3/puhelinluettelonbackend/index.js
+++ osa3/puhelinluettelonbackend/index.js
@@ -6,6 +6,7 @@ app.use(express.json())
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
+app.use(express.static('dist'))
 
 let persons = [
     {
---
+++ readme.md
@@ -0,0 +1,1 @@
+https://puhelinluettelonbackend-ptvp.onrender.com
